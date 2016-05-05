9c70bb5a3e2778bddd3b2caefcd2b803f8bd5c3e
Scott McLeod <halcyonblue@gmail.com>
2016-05-05 11:10:39 -0400

    Update Primes to use java8

--- src/main/java/com/deepresolve/fundamentals/Core.java
+++ src/main/java/com/deepresolve/fundamentals/Core.java
@@ -1,28 +1,29 @@
 package com.deepresolve.fundamentals;
 
 import java.util.stream.IntStream;
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Core {
     public static int divisorsSum(int num) {
         // return IntStream.range(2, num/2 + 1).filter(i -> num % i == 0);
         return IntStream.range(1, num + 1).filter(i -> num % i == 0).sum();
     }
-    public static void primes() {
-        int num=50;
-        int count=0;
-        for(int i=1; i<= num; i++) {
-            count=0;
-            for(int j=2; j<=i/2; j++) {
-                if(i%j==0) {
-                    count++;
-                }
-            }
-            if(count == 0) {
-                System.out.println(i);
-            }
-        }
 
+    Collection<Integer> primes(int numPrimes) {
+        final List<Integer> primes = new ArrayList<>(numPrimes);
+        IntStream.iterate(2, i -> i + 1).
+            filter(i -> {
+                    for (int prime : primes)
+                        if (i % prime == 0)
+                            return false;
+                    return true;
+                }).limit(numPrimes).
+            forEach(primes::add);
+        return primes;
     }
+
     public static void main(String[] args) {
         //properDivisors(12).forEach(element -> System.out.println(element));
         System.out.println(divisorsSum(6));
--- src/test/java/com/deepresolve/fundamentals/CoreSpec.java
+++ src/test/java/com/deepresolve/fundamentals/CoreSpec.java
@@ -1,11 +1,25 @@
 package com.deepresolve.fundamentals;
 
-import org.testng.Assert;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.Arrays;
 
 public class CoreSpec {
+    private Core sut;
+
+    @BeforeClass
+    public void setupClass() {
+        sut = new Core();
+    }
+
+    @Test
+    public void testPrimes() {
+        assertEquals(Arrays.asList(2,3,5,7,11),sut.primes(5));
+    }
     @Test
     public void testDivisorsSum() {
-        Assert.assertEquals(12, Core.divisorsSum(6));
+        assertEquals(12, Core.divisorsSum(6));
     }
 }
