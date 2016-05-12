35ec47379837808b7b6f7500538055d8697403f4
Scott McLeod <halcyonblue@gmail.com>
2016-05-12 19:51:29 -0400

    Adding sort

--- src/main/java/com/deepresolve/fundamentals/Core.java
+++ src/main/java/com/deepresolve/fundamentals/Core.java
@@ -30,6 +30,45 @@ public class Core {
         System.out.println(sc.next());
     }
 
+    void swap(int[] nums, int i, int j) {
+        int left = nums[i];
+        int right = nums[j];
+        nums[i] = right;
+        nums[j] = left;
+    }
+
+    int[] sortInts(int[] nums) {
+        int totalSwaps = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int numberOfSwaps = 0;
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                    numberOfSwaps++;
+                    totalSwaps++;
+                }
+            }
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        return new int[]{totalSwaps,nums[0],nums[nums.length-1]};
+    }
+
+    void sort() {
+        Scanner scan = new Scanner(System.in);
+        int length = scan.nextInt();
+        int[] array = new int[length];
+        int[] results = new int[3];
+        for(int i = 0 ; i < length; i++) {
+            array[i] = scan.nextInt();
+        }
+        results = sortInts(array);
+        System.out.println("Array is sorted in " + results[0] + " swaps.");
+        System.out.println("First Element: " + results [1]);
+        System.out.println("Last Element: " + results [2]);
+    }
+
     public static void main(String[] args) {
         //properDivisors(12).forEach(element -> System.out.println(element));
         //        System.out.println(divisorsSum(6));
--- src/test/java/com/deepresolve/fundamentals/CoreSpec.java
+++ src/test/java/com/deepresolve/fundamentals/CoreSpec.java
@@ -20,14 +20,35 @@ public class CoreSpec {
         assertEquals(Arrays.asList(2,3,5,7,11),
                      sut.primes(5));
     }
+
+    @Test
+    public void swapTest() {
+        int[] a = new int[]{1,2,3,4};
+        sut.swap(a,1,2);
+        assertEquals(new int[]{1,3,2,4},a);
+    }
+
+    @Test
+    public void sortIntsTest() {
+        assertEquals(new int[]{0,1,3},sut.sortInts(new int[]{1,2,3}));
+        assertEquals(new int[]{3,1,3},sut.sortInts(new int[]{3,2,1}));
+    }
+
     @Test
     public void divisorsSumTest() {
         assertEquals(12, Core.divisorsSum(6));
     }
 
     @Test
+    public void sortTest() {
+        assertEquals(Util.wrapIO(sut::sort,"3\n" + "1 2 3\n"),
+                     "Array is sorted in 0 swaps.\n" +
+                     "First Element: 1\n" +
+                     "Last Element: 3\n");
+    }
+
+    @Test
     public void IOtest(){
-        assertEquals("55\n",
-                     (Util.wrapIO(sut::IO,"55")));
+        assertEquals("55\n",Util.wrapIO(sut::IO,"55"));
     }
 }
